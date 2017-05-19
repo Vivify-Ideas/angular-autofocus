6c68eb41f92c99d618d87a917cdfd6e7dc250668
papic <danilo.p@vivifyideas.com>
2017-05-19 15:44:59 +0200

    Added module export.

--- src/autofocus.js
+++ src/autofocus.js
@@ -1,6 +1,20 @@
-angular.module('vivify-ideas.autofocus', [])
-
-    .directive('autofocus', ['$timeout', function($timeout) {
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['angular'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory(require('angular'));
+    } else {
+        // Browser globals (root is window)
+        root.returnExports = factory(root.angular);
+    }
+}(this, function (angular) {
+    var moduleName = 'vivify-ideas.angular-autofocus';
+    var mod = angular.module(moduleName, []);
+    mod.directive('vi-autofocus', ['$timeout', function($timeout) {
         return {
             restrict: 'A',
             link : function($scope, $element) {
@@ -10,3 +24,6 @@ angular.module('vivify-ideas.autofocus', [])
             }
         }
     }]);
+
+    return moduleName;
+}));
