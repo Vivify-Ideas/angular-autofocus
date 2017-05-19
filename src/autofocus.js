1ed259739eb4ba068ebea2d0b5144df5fef794f4
papic <danilo.p@vivifyideas.com>
2017-05-19 14:35:07 +0200

    Initial directive code added.

---
+++ src/autofocus.js
@@ -0,0 +1,12 @@
+angular.module('vivify-ideas.autofocus', [])
+
+    .directive('autofocus', ['$timeout', function($timeout) {
+        return {
+            restrict: 'A',
+            link : function($scope, $element) {
+                $timeout(function() {
+                    $element[0].focus();
+                });
+            }
+        }
+    }]);
